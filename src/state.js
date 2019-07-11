bff04a849c616bfdb815e57b036269e0e13d4038
Edward Irby <edward@dx.works>
2019-07-11 09:31:15 -0700

    --wip-- [skip ci]

--- src/state.js
+++ src/state.js
@@ -2,18 +2,15 @@ const worker = new Worker('./redux.worker.js', { type: 'module' })
 let listeners = new Map();
 const fireListeners = ({ data }) => {
   const { address, props } = data
-  const queue = listeners.get(address) || []
-  queue.forEach(l => l(props));
+  const fn = listeners.get(address)
+  fn(props);
 }
 
 worker.addEventListener('message', fireListeners, { passive: true });
 export const  connect = (address, fn) =>{
-  const queue = listeners.has(address)
-    ? listeners.get(address)
-    : listeners.set(address, []).get(address)
-  !queue.includes(fn) && queue.push(fn);
+  !listeners.has(address) && listeners.set(address, fn)
   return () => {
-    queue.filter(l => l !== fn)
+    listeners.delete(address)
   }
 }
 export const dispatch = (action) =>{
